ef0bbea9e8dcdb2d565a5abe9a95252f570010cb
Khaleedabuhawwas92 <42714078+Khaleedabuhawwas92@users.noreply.github.com>
2022-01-23 13:56:47 +0200

    uplode222

--- db.js
+++ db.js
@@ -3,10 +3,12 @@ const artc = require("./models/Article");
 // mongodb database connection string. change it as per your needs. here "mydb" is the name of the database. You don't need to create DB from mongodb terminal. mongoose create the db automatically.
 //mongodb+srv://<username>:<password>@database.jbeyu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 mongoose.connect(
-  "mongodb+srv://Khaled:khaleed2010709307@khaleed.jbeyu.mongodb.net/test?authSource=admin&replicaSet=atlas-11jbls-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
+  "mongodb+srv://doadmin:L823op17hx6H5m4c@db-mongodb-fra1-16206-ba751473.mongo.ondigitalocean.com/admin?authSource=admin&replicaSet=db-mongodb-fra1-16206&tls=true",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    tls: true,
+    tlsCAFile: './ca-certificate.crt',
   }
 );
 var db = mongoose.connection;
